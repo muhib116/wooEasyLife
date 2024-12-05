880669440773e2619f8a8f5b65b39a3ab26b3e9f
Muhibbullah Ansary <muhibbullah611@gmail.com>
2024-12-05 16:13:24 +0600

    working otp validation

--- frontend/OTPValidatorForOrderPlace.php
+++ frontend/OTPValidatorForOrderPlace.php
@@ -4,9 +4,18 @@ namespace WooEasyLife\Frontend;
 class OTPValidatorForOrderPlace {
     function __construct()
     {
-        echo '<h3 style="margin-bottom: 15px; font-weight: bold; text-align: center;">
-            Review Your Order Before Placing
-        </h3>';
-
+        // add_action('wp_footer', [$this, 'wooeasylife_check_if_checkout_page']);
+        add_filter('woocommerce_order_button_html', [$this, 'customize_place_order_button'], 30);
+        // add_filter('proceedToCheckoutButtonLabel', [$this, 'customize_place_order_button']);
     }
+
+    function customize_place_order_button($button_html) {
+        // Customize the button text or add extra attributes
+        $custom_button = '<button type="submit" class="button alt custom-class" name="woocommerce_checkout_place_order" id="place_order" value="Place Order Now">';
+        $custom_button .= 'Confirm & Pay'; // Custom text for the button
+        $custom_button .= '</button>';
+    
+        return $custom_button;
+    }
+    
 }
